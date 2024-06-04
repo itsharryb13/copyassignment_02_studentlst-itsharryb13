d8e34b8751c4393d6612bb7b9a98889bffdeae44
Harpreet Singh <90818751+itsharryb13@users.noreply.github.com>
2024-06-04 18:52:26 -0400

    cloned files

---
+++ assignment_02_studentlst-itsharryb13/src/main/java/com/mycompany/studentlst/Student.java
@@ -0,0 +1,31 @@
+package com.mycompany.studentlst;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class Student {
+    
+    private String name;
+
+    //To Do 01: add a constructor to this class
+    public Student() {
+        this.name = null;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+
+    // To Do 02: Add setters and getters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ assignment_02_studentlst-itsharryb13/src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -0,0 +1,43 @@
+package com.mycompany.studentlst;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author MoaathAlrajab
+ */
+public class StudentLst {
+
+    public static void main(String[] args) {
+        Student[] students= new Student[2];
+        Scanner inScanner = new Scanner(System.in);
+        
+        for(int i=0; i< students.length;i++){
+            System.out.print("Enter student's name: ");
+            String name = inScanner.nextLine();
+            Student student = new Student(name);
+            
+            students[i]= student;
+            
+        }
+        System.out.println(printOutput(students));
+    }
+    
+    public static String printOutput(Student[] students){
+
+        String str="";
+
+        if (students[0].getName() == null){
+            str = null;
+        }else{
+            str = students[0].getName();
+        }
+
+        
+        //To Do 03: complete the implementation of this method  
+        
+        return str;
+    }
+    
+    
+}
